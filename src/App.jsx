0bca2c269c213e3ff40d69cfae1b848adc9a8a94
AKSHAY GOPAN <akshaygopan377@gmail.com>
2024-04-07 04:58:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [weatherImage, setWeatherImage] = useState("");
+
+  function getWeather() {
+    const apiKey = "51706f8fb5e8f97484e919fba83dca8b";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let MT = Math.round(data.main.temp);
+        let FL = Math.round(data.main.feels_like);
+
+        const weather = {
+          location: `${data.name}`,
+          temperature: `${Math.round(MT)}°C`,
+          feels_like: `${FL} °C`,
+          humidity: `${data.main.humidity} %`,
+          wind: `${data.wind.speed} km/h`,
+          condition: `${data.weather[0].main}`,
+        };
+
+        setWeatherInfo(weather);
+        setWeatherImage(getImage(data.weather[0].main));
+      });
+  }
+
+  function getImage(type) {
+    if (type == "Clear") {
+      return "./src/assets/images/clear.png";
+    }
+    if (type == "Clouds") {
+      return "./src/assets/images/clouds.png";
+    }
+    if (type == "Drizzle") {
+      return "./src/assets/images/drizzle.png";
+    }
+    if (type == "Humidity") {
+      return "./src/assets/images/humidity.png";
+    }
+    if (type == "Mist" || "Haze") {
+      return "./src/assets/images/mist.png";
+    }
+    if (type == "Rain") {
+      return "./src/assets/images/rain.png";
+    }
+    if (type == "Snow") {
+      return "./src/assets/images/snow.png";
+    }
+    if (type == "Wind") {
+      return "./src/assets/images/wind.png";
+    }
+  }
+
+  return (
+    <div className="card">
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter City Name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>search</button>
+      </div>
+      {weatherInfo && (
+        <div className=" row">
+          <div className="weather col">
+            <h1 className="temp">{weatherInfo.temperature}</h1>
+            <h3 className="city">{weatherInfo.location}</h3>
+            <img src={weatherImage} alt="image" className="weather-icon" />
+            <h5>{weatherInfo.condition}</h5>
+          </div>
+
+          <div className=" details col">
+            <div className="innercard card">
+              <p>Humidity</p>
+              <h4 className="humidity">{weatherInfo.humidity}</h4>
+            </div>
+            <div className="line"></div>
+            <div className="innercard card">
+              <p>Wind speed</p>
+              <h4 className="wind">{weatherInfo.wind}</h4>
+            </div>
+            <div className="line"></div>
+            <div className="innercard card">
+              <p>Feels like</p>
+              <h4 className="feels_like">{weatherInfo.feels_like}</h4>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
